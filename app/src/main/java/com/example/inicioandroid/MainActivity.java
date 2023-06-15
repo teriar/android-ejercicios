881db0111b5bad1493394232783e07481bda9a51
Felipe Gonzalez <felipegonzalezurr@gmail.com>
2023-06-14 21:25:57 -0400

    ingrso texview

--- app/src/main/java/com/example/inicioandroid/MainActivity.java
+++ app/src/main/java/com/example/inicioandroid/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.inicioandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TextView text = findViewById(R.id.mainText);
+        int result = calcular("3,6");
+
+        //text.setText("hola mundo");
+    }
+
+    private int calcular(String numeros){
+        if(numeros.isEmpty()){
+            return 0;
+        }
+        String[] num = numeros.split(",");
+        int sum =0;
+        for(int i =0; i< num.length; i++){
+            sum +=  Integer.valueOf(num[i]);
+        }
+        return sum;
     }
 }
